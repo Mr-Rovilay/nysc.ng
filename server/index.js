f82713e11dbfe08af8c17a7a2aada3722aa0c7ad
Mr-Rovilay <ayodejiakinola007@gmail.com>
2024-05-20 13:06:48 +0100

    added auth

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import connectDB from "./db/db.js";
 import authRoutes from "./routes/authRoutes.js";
-import userRoutes from "./routes/userRoutes.js";
 import "dotenv/config";
 
 const app = express();
@@ -14,7 +13,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/auth", authRoutes);
-app.use("/user", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("OK...my message");
