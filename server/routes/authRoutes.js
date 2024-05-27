8a499fb336dffea10e50fedaf444265950bda49e
Mr-Rovilay <ayodejiakinola007@gmail.com>
2024-05-27 10:24:09 +0100

    hero-section

--- server/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import authControllers from "../controllers/authControllers.js";
-const router = express();
-
-router.post("/signup", authControllers.signUp);
-router.post("/signin", authControllers.signIn);
-
-export default router;
