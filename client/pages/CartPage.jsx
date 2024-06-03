2e623f6aa213f4aa214f1483b230f05914bdd7e4
Mr-Rovilay <ayodejiakinola007@gmail.com>
2024-06-03 19:48:02 +0100

    hero-section

--- client/pages/CartPage.jsx
+++ client/pages/CartPage.jsx
@@ -20,6 +20,7 @@ const CartPage = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
   const [totalPrice, setTotalPrice] = useState(0);
+  const [cartVisible, setCartVisible] = useState(true);
 
   useEffect(() => {
     const calculateTotalPrice = () => {
@@ -63,204 +64,220 @@ const CartPage = () => {
       <ToastContainer />
       <div className="min-h-screen bg-gray-100">
         <div className="p-5 sm:p-2">
-          <h1 className="text-3xl font-light text-center mb-8">YOUR CART</h1>
-          <div className="relative flex flex-col sm:flex-row items-center justify-between p-5 bg-white rounded-lg shadow-md mb-8">
-            <Link to={"/products"}>
-              <button className="px-4 py-2 font-semibold border border-gray-300 rounded-lg bg-transparent hover:bg-gray-100 transition duration-300 mb-2 sm:mb-0">
-                CONTINUE SHOPPING
-              </button>
-            </Link>
-            <div className="flex flex-col sm:flex-row sm:items-center mb-2 sm:mb-0">
-              <div className="underline cursor-pointer mx-2 mb-2 sm:mb-0">
-                Shopping Bag({cart.products?.length || 0})
+          {cartVisible ? (
+            <div>
+              <h1 className="text-3xl font-light text-center mb-8">
+                YOUR CART
+              </h1>
+              <div className="relative flex flex-col sm:flex-row items-center justify-between p-5 bg-white rounded-lg shadow-md mb-8">
+                <Link to={"/products"}>
+                  <button className="px-4 py-2 font-semibold border border-gray-300 rounded-lg bg-transparent hover:bg-gray-100 transition duration-300 mb-2 sm:mb-0">
+                    CONTINUE SHOPPING
+                  </button>
+                </Link>
+
+                <div className="flex flex-col sm:flex-row sm:items-center mb-2 sm:mb-0">
+                  <div className="underline cursor-pointer mx-2 mb-2 sm:mb-0">
+                    Shopping Bag({cart.products?.length || 0})
+                  </div>
+                </div>
+                <button className="px-4 py-2 font-semibold text-white bg-black rounded-lg shadow-md hover:bg-gray-800 transition duration-300">
+                  CHECKOUT NOW
+                </button>
+                {cart.products?.length > 0 && (
+                  <div className="absolute top-0 right-0 mt-2 mr-2">
+                    <button
+                      className="px-4 py-2 font-semibold text-white bg-black rounded-lg shadow-md hover:bg-gray-800 transition duration-300"
+                      onClick={handleClearCart}
+                    >
+                      Clear cart
+                    </button>
+                  </div>
+                )}
+              </div>
+
+              <Card className="h-full w-full overflow-scroll mb-8">
+                <table className="w-full min-w-max table-auto text-left">
+                  <thead>
+                    <tr>
+                      <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal leading-none opacity-70"
+                        >
+                          #
+                        </Typography>
+                      </th>
+                      <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal leading-none opacity-70"
+                        >
+                          Image
+                        </Typography>
+                      </th>
+                      <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal leading-none opacity-70"
+                        >
+                          Product Name
+                        </Typography>
+                      </th>
+                      <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal leading-none opacity-70"
+                        >
+                          Quantity
+                        </Typography>
+                      </th>
+                      <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal leading-none opacity-70"
+                        >
+                          Price
+                        </Typography>
+                      </th>
+                      <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal leading-none opacity-70"
+                        >
+                          Action
+                        </Typography>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {cart.products?.map((item, index) => (
+                      <tr key={index}>
+                        <td className="p-4">
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal"
+                          >
+                            {index + 1}
+                          </Typography>
+                        </td>
+                        <td className="p-4">
+                          <div className="rounded-full w-12 h-12 overflow-hidden">
+                            <img
+                              src={item.productId.image}
+                              alt={item.productId.title}
+                              className="w-full h-full object-cover"
+                            />
+                          </div>
+                        </td>
+                        <td className="p-4">
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal capitalize"
+                          >
+                            {item.productId.title}
+                          </Typography>
+                        </td>
+                        <td className="p-4">
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal capitalize"
+                          >
+                            <button
+                              className="btn text-xl"
+                              onClick={() =>
+                                handleDecreaseQuantity(item.productId._id)
+                              }
+                            >
+                              -
+                            </button>
+                            <input
+                              type="number"
+                              value={item.quantity}
+                              className="w-10 mx-2 text-center overflow-hidden appearance-none"
+                              readOnly
+                            />
+                            <button
+                              className="btn text-xl"
+                              onClick={() =>
+                                handleIncreaseQuantity(item.productId._id)
+                              }
+                            >
+                              +
+                            </button>
+                          </Typography>
+                        </td>
+                        <td className="p-4">
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal"
+                          >
+                            {item.productId.price}
+                          </Typography>
+                        </td>
+                        <td className="p-4">
+                          <button
+                            className="btn hover:bg-red-600"
+                            onClick={() => deleteCartItem(item.productId._id)}
+                          >
+                            <FaTrashAlt className="text-red-500" />
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </Card>
+
+              <div className="flex flex-col md:flex-row justify-between items-start my-12 gap-8">
+                <div className="md:w-1/2 space-y-3">
+                  <h3 className="text-lg font-semibold">Shopping Details</h3>
+                  <p>Total Items: {cart.products?.length || 0}</p>
+                  <p>
+                    Total Price:{" "}
+                    <span id="total-price">${totalPrice.toFixed(2)}</span>
+                  </p>
+                  <Link to="/delivery-info">
+                    <div className="mt-4">
+                      <Button
+                        variant="secondary"
+                        text={"proceed to checkout"}
+                      />
+                    </div>
+                  </Link>
+                </div>
+              </div>
+
+              {/* Back to Menu Button */}
+              <div className="flex items-center justify-center mt-4">
+                <Link to="/products">
+                  <Button variant="secondary" text={"back to products"} />
+                </Link>
+              </div>
+
+              <div className="flex justify-center mt-6">
+                <Pagination
+                  currentPage={currentPage}
+                  totalPages={totalPages}
+                  onPageChange={handlePageChange}
+                />
               </div>
             </div>
-            <button className="px-4 py-2 font-semibold text-white bg-black rounded-lg shadow-md hover:bg-gray-800 transition duration-300">
-              CHECKOUT NOW
-            </button>
-            <div className="absolute top-0 right-0 mt-2 mr-2">
-              <button
-                className="px-4 py-2 font-semibold text-white bg-black rounded-lg shadow-md hover:bg-gray-800 transition duration-300"
-                onClick={handleClearCart}
-              >
-                Clear cart
-              </button>
+          ) : (
+            <div className="text-center mt-20">
+              {cart.length > 0 && <p>Cart is empty. Please add products.</p>}
             </div>
-          </div>
-
-          <Card className="h-full w-full overflow-scroll mb-8">
-            <table className="w-full min-w-max table-auto text-left">
-              <thead>
-                <tr>
-                  <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
-                    <Typography
-                      variant="small"
-                      color="blue-gray"
-                      className="font-normal leading-none opacity-70"
-                    >
-                      #
-                    </Typography>
-                  </th>
-                  <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
-                    <Typography
-                      variant="small"
-                      color="blue-gray"
-                      className="font-normal leading-none opacity-70"
-                    >
-                      Image
-                    </Typography>
-                  </th>
-                  <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
-                    <Typography
-                      variant="small"
-                      color="blue-gray"
-                      className="font-normal leading-none opacity-70"
-                    >
-                      Product Name
-                    </Typography>
-                  </th>
-                  <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
-                    <Typography
-                      variant="small"
-                      color="blue-gray"
-                      className="font-normal leading-none opacity-70"
-                    >
-                      Quantity
-                    </Typography>
-                  </th>
-                  <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
-                    <Typography
-                      variant="small"
-                      color="blue-gray"
-                      className="font-normal leading-none opacity-70"
-                    >
-                      Price
-                    </Typography>
-                  </th>
-                  <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
-                    <Typography
-                      variant="small"
-                      color="blue-gray"
-                      className="font-normal leading-none opacity-70"
-                    >
-                      Action
-                    </Typography>
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {cart.products?.map((item, index) => (
-                  <tr key={index}>
-                    <td className="p-4">
-                      <Typography
-                        variant="small"
-                        color="blue-gray"
-                        className="font-normal"
-                      >
-                        {index + 1}
-                      </Typography>
-                    </td>
-                    <td className="p-4">
-                      <div className="rounded-full w-12 h-12 overflow-hidden">
-                        <img
-                          src={item.productId.image}
-                          alt={item.productId.title}
-                          className="w-full h-full object-cover"
-                        />
-                      </div>
-                    </td>
-                    <td className="p-4">
-                      <Typography
-                        variant="small"
-                        color="blue-gray"
-                        className="font-normal capitalize"
-                      >
-                        {item.productId.title}
-                      </Typography>
-                    </td>
-                    <td className="p-4">
-                      <Typography
-                        variant="small"
-                        color="blue-gray"
-                        className="font-normal capitalize"
-                      >
-                        <button
-                          className="btn text-xl"
-                          onClick={() =>
-                            handleDecreaseQuantity(item.productId._id)
-                          }
-                        >
-                          -
-                        </button>
-                        <input
-                          type="number"
-                          value={item.quantity}
-                          className="w-10 mx-2 text-center overflow-hidden appearance-none"
-                          readOnly
-                        />
-                        <button
-                          className="btn text-xl"
-                          onClick={() =>
-                            handleIncreaseQuantity(item.productId._id)
-                          }
-                        >
-                          +
-                        </button>
-                      </Typography>
-                    </td>
-                    <td className="p-4">
-                      <Typography
-                        variant="small"
-                        color="blue-gray"
-                        className="font-normal"
-                      >
-                        {item.productId.price}
-                      </Typography>
-                    </td>
-                    <td className="p-4">
-                      <button
-                        className="btn hover:bg-red-600"
-                        onClick={() => deleteCartItem(item.productId._id)}
-                      >
-                        <FaTrashAlt className="text-red-500" />
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </Card>
-
-          <div className="flex flex-col md:flex-row justify-between items-start my-12 gap-8">
-            <div className="md:w-1/2 space-y-3">
-              <h3 className="text-lg font-semibold">Shopping Details</h3>
-              <p>Total Items: {cart.products?.length || 0}</p>
-              <p>
-                Total Price:{" "}
-                <span id="total-price">${totalPrice.toFixed(2)}</span>
-              </p>
-              <Link to="/delivery-info">
-                <div className="mt-4">
-                  <Button variant="secondary" text={"proceed to checkout"} />
-                </div>
-              </Link>
-            </div>
-          </div>
-
-          {/* Back to Menu Button */}
-          <div className="flex items-center justify-center mt-4">
-            <Link to="/products">
-              <Button variant="secondary" text={"back to products"} />
-            </Link>
-          </div>
-
-          <div className="flex justify-center mt-6">
-            <Pagination
-              currentPage={currentPage}
-              totalPages={totalPages}
-              onPageChange={handlePageChange}
-            />
-          </div>
+          )}
         </div>
       </div>
     </div>
