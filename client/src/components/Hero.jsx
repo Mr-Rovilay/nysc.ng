c9dc23dddddc43932d66f5f6744efcc2b4bd3224
Mr-Rovilay <ayodejiakinola007@gmail.com>
2024-06-09 21:13:50 +0100

    master

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,6 +1,21 @@
-import Button from "./Button";
+const Button = ({ text, variant, onClick }) => {
+  return (
+    <button
+      className={`text-white font-semibold py-2 px-4 rounded ${
+        variant === "primary" ? "bg-blue-500" : "bg-gray-500"
+      }`}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+};
 
 const Hero = () => {
+  const openWhatsApp = () => {
+    window.open("https://api.whatsapp.com/send?text=text&phone=+2348140570029");
+  };
+
   return (
     <div className="relative bg-gradient-to-r h-screen bg-gray-100 text-white overflow-hidden">
       <div className="absolute inset-0 flex flex-col justify-center items-center text-center">
@@ -18,9 +33,11 @@ const Hero = () => {
         </div>
         <p className="mt-4 text-black">or</p>
         <div className="mt-4">
-          <Button variant={"secondary"} text={"Send Message on WhatsApp"}>
-            <a href="https://api.whatsapp.com/send?text=text&phone=+2348140570029"></a>
-          </Button>
+          <Button
+            variant={"secondary"}
+            text={"Send Message on WhatsApp"}
+            onClick={openWhatsApp}
+          />
         </div>
       </div>
     </div>
