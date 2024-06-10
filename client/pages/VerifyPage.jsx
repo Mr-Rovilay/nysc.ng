9ed7462d76326f6a4e1f86aa0362c12fd8bd3ad2
Mr-Rovilay <ayodejiakinola007@gmail.com>
2024-06-10 16:48:58 +0100

    hello

--- client/pages/VerifyPage.jsx
+++ client/pages/VerifyPage.jsx
@@ -12,24 +12,19 @@ const VerifyPage = () => {
   const navigate = useNavigate();
 
   const verifyPayment = async () => {
-    console.log("Starting payment verification...");
     try {
       const response = await publicRequest.post("/orders/verify", {
         success,
         orderId,
       });
 
-      console.log("Verification response:", response.data);
-
       if (response.data.success) {
-        console.log("Verification successful, navigating to /myOrders");
         navigate("/myOrders");
       } else {
         toast.error("Order verification failed");
         navigate("/");
       }
     } catch (error) {
-      console.error("Payment verification failed:", error);
       toast.error("Payment verification failed");
       navigate("/");
     }
