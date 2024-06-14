56d33c465f3d0816c701aacf6d98f8b5144c38a5
Mr-Rovilay <ayodejiakinola007@gmail.com>
2024-06-14 17:39:14 +0100

    master

--- client/src/data.js
+++ client/src/data.js
@@ -9,12 +9,12 @@ export const categories = [
     id: 2,
     img: "https://cdn.punchng.com/wp-content/uploads/2023/03/24215813/Ruth-Innocent.jpg",
     title: "MALE & FEMALE",
-    categories: "Male and Female",
+    categories: "male and female",
   },
   {
     id: 3,
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxh3aa0PaCqM0kkeBe0tvvvCijlLid1AXgKQ&s",
     title: "MALE STYLE",
-    categories: "Male",
+    categories: "male",
   },
 ];
